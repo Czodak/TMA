30d599c8e2b19793f13a0c369f6a5b82e2482ac9
Czodak <bartekch99@gmail.com>
2025-03-16 14:41:48 +0100

    initialize architecture and projects

---
+++ NotificationService/Worker.cs
@@ -0,0 +1,23 @@
+namespace NotificationService
+{
+    public class Worker : BackgroundService
+    {
+        private readonly ILogger<Worker> _logger;
+
+        public Worker(ILogger<Worker> logger)
+        {
+            _logger = logger;
+        }
+
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            while (!stoppingToken.IsCancellationRequested)
+            {
+                if (_logger.IsEnabled(LogLevel.Information))
+                {
+                }
+                await Task.Delay(10000, stoppingToken);
+            }
+        }
+    }
+}
---
+++ TaskApi/Controllers/HealthCheck.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TaskApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HealthCheck : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok();
+        }
+    }
+}
